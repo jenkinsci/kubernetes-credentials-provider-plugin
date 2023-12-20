238c5c2764f3d332e59ceaf49112c2cbed390b52
Tue Dissing <tue.dissing@ufst.dk>
2023-12-20 09:42:06 +0100

    initial changes made

---
+++ src/main/java/com/cloudbees/jenkins/plugins/kubernetes_credentials_provider/convertors/AzureServicePrincipalCedentialsConvertor.java
@@ -0,0 +1,71 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 CloudBees, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.convertors;
+
+
+import org.jenkinsci.plugins.variant.OptionalExtension;
+
+import com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.CredentialsConvertionException;
+import com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.SecretToCredentialConverter;
+import com.cloudbees.jenkins.plugins.kubernetes_credentials_provider.SecretUtils;
+import com.cloudbees.plugins.credentials.CredentialsScope;
+import com.microsoft.azure.util.AzureCredentials;
+
+import io.fabric8.kubernetes.api.model.Secret;
+
+/**
+ * SecretToCredentialConvertor that converts {@link om.microsoft.azure.util.AzureCredentials}.
+ */
+@OptionalExtension(requirePlugins={"azure-credentials"})
+public class AzureServicePrincipalCedentialsConvertor extends SecretToCredentialConverter {
+
+    @Override
+    public boolean canConvert(String type) {
+        return "azureServicePrincipal".equals(type);
+    }
+
+    @Override
+    public AzureCredentials convert(Secret secret) throws CredentialsConvertionException {
+    	// ensure we have some data
+        SecretUtils.requireNonNull(secret.getData(), "azureCredentials kubernetes definition contains no data");
+        
+        String credsId = SecretUtils.getCredentialId(secret);
+        String description = SecretUtils.getCredentialDescription(secret);
+
+        // Assuming this is a service principal creds type
+        String subscriptionId = SecretUtils.getNonNullSecretData(secret, "subscripitonId", "azureCredentials service principal credential is missing the subscriptionId");
+        String clientId = SecretUtils.getNonNullSecretData(secret, "clientId", "azureCredentials service principal credential is missing the clientId");
+        String clientSecret = SecretUtils.getNonNullSecretData(secret, "clientSecret", "azureCredentials service principal credential is missing the clientSecret");
+        String tenantId = SecretUtils.getNonNullSecretData(secret, "tenantId", "azureCredentials service principal credential is missing the tenantId");
+        
+        
+        AzureCredentials azureCredentials = new AzureCredentials(CredentialsScope.SYSTEM, credsId, description, subscriptionId, clientId, hudson.util.Secret.fromString(clientSecret));
+        // Configure credentials against the correct Azure environment
+        azureCredentials.setTenant(tenantId);
+        azureCredentials.setAzureEnvironmentName("Azure");
+
+    	return azureCredentials;
+    }
+
+}
